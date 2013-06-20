60f81dd001b72159a362260ffb7233c6678618d7
master94 <shevchuk.valery@gmail.com>
2013-06-21 00:33:41 +0300

    - test commit

---
+++ src/test.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: master
+ * Date: 6/21/13
+ * Time: 12:33 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+}
